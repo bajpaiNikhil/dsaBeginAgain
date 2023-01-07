5220a6f85b47343ae20be14766950cb5b19ae7d8
NikhilBajpai <nikhil.cse16@gmail.com>
2023-01-07 15:08:11 +0530

    codeChef stater questions

--- Average Permutation.py
+++ Average Permutation.py
@@ -11,14 +11,3 @@ for t in range(t):
         else:
             even.append(i)
     print(str(odd[::-1]+even)[1:-1].replace(",",""))
-
-n = 10
-odd = []
-even = []
-
-for i in range(1, n):
-    if i & 1 != 0:
-        odd.append(i)
-    else:
-        even.append(i)
-print(even, odd)
